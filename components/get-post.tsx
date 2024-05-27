28373cf595a0851ad7ad751b694745a86757234d
jobcho <jobchojiyo@gmail.com>
2024-05-27 17:05:18 +0900

    Update get-post.tsx

--- components/get-post.tsx
+++ components/get-post.tsx
@@ -1,6 +1,6 @@
 import { API_URL } from "../app/constants";
 
-export async function getPost(id: string) {
+export default async function getPost(id: string) {
   const response = await fetch(`${API_URL}/post/${id}`);
   return response.json();
 }
